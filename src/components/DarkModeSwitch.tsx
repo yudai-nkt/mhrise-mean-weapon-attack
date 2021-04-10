1f696ee793cad1ae64eb47b13b23720659995a22
Yudai NAKATA <yudai.nkt@gmail.com>
2021-04-11 02:47:13 +0900

    feat: make dark mode toggle more intuitive

--- src/components/DarkModeSwitch.tsx
+++ src/components/DarkModeSwitch.tsx
@@ -1,16 +1,24 @@
-import { useColorMode, Switch } from "@chakra-ui/react";
+import { useColorMode, IconButton } from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 
 export const DarkModeSwitch = (): JSX.Element => {
   const { colorMode, toggleColorMode } = useColorMode();
   const isDark = colorMode === "dark";
   return (
-    <Switch
+    <IconButton
+      aria-label="Toggle dark mode"
       position="fixed"
       top="1rem"
       right="1rem"
-      color="green"
-      isChecked={isDark}
-      onChange={toggleColorMode}
+      size="sm"
+      isRound={true}
+      color={isDark ? "orange.500" : "yellow.300"}
+      backgroundColor={isDark ? "gray.50" : "gray.700"}
+      _hover={{ backgroundColor: isDark ? "gray.200" : "gray.900" }}
+      icon={
+        isDark ? <SunIcon boxSize="1.25em" /> : <MoonIcon boxSize="1.25em" />
+      }
+      onClick={toggleColorMode}
     />
   );
 };
